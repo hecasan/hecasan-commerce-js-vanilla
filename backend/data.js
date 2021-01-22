88d267d295162b37a5b467c9bc807cc2e2f441f7
Héte Caetano <hetecaetano@gmail.com>
2021-01-22 17:02:55 -0300

    Estilização da página do produto

--- backend/data.js
+++ backend/data.js
@@ -10,6 +10,8 @@ module.exports = {
       rating: 4.5,
       numReviews: 10,
       countInStock: 6,
+      shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
       _id: '2',
@@ -20,6 +22,8 @@ module.exports = {
       brand: 'Nike',
       rating: 5,
       numReviews: 51,
+      shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
       _id: '3',
@@ -30,6 +34,8 @@ module.exports = {
       brand: 'Adidas',
       rating: 4,
       numReviews: 45,
+      shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
       _id: '4',
@@ -41,6 +47,8 @@ module.exports = {
       rating: 2.5,
       numReviews: 198,
       countInStock: 6,
+      shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
       _id: '5',
@@ -52,6 +60,8 @@ module.exports = {
       rating: 3.5,
       numReviews: 89,
       countInStock: 6,
+      shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
       _id: '6',
@@ -63,6 +73,8 @@ module.exports = {
       rating: 4.5,
       numReviews: 45,
       countInStock: 6,
+      shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
   ],
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import { getProduct } from '../../api';
+import RatingComponent from '../components/Rating';
 import { parseRequestUrl } from "../utils";
 
 const ProductScreen = {
@@ -8,7 +9,56 @@ const ProductScreen = {
     if (product.error) {
       return `<div>${product.error}</div>`;
     }
-    return `<h1>${product.name}</h1>`;
+    return `
+    <div class="content">
+      <div class="back-to-result">
+        <a href="/#/">Voltar para produtos </a>
+      </div>
+      <div class="details">
+        <div class="details-image">
+          <img src="${product.image}" alt="${product.name}" />
+        </div>
+        <div class="details-info">
+          <ul>
+            <li>
+              <h1>${product.name}</h1>
+            </li>
+            <li>
+            ${RatingComponent.render({
+      value: product.rating,
+      text: `${product.numReviews} visualizações`,
+    })}
+            </li>
+            <li>
+              Preço: <strong>R$ ${product.price}</strong>
+            </li>
+            <li>
+              Descrição:
+              <div>
+              <p class="shortDescription">${product.shortDescription}</p>
+                
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="details-action">
+            <ul>
+              <li>
+                Preço: R$ ${product.price}
+              </li>
+              <li>
+                Estoque: 
+                  ${product.countInStock > 0
+        ? `<span class="success">Produto em estoque - Qtde: ${product.countInStock}</span>`
+        : `<span class="error">Produto indisponivel</span>`
+      }
+              </li>
+              <li>
+                  <button id="add-button" class="fw primary">Add to Cart </div>
+            </ul>
+        </div>
+      </div>
+    </div>`;
   },
 };
 export default ProductScreen;
