facbd9aff78cf25140fc866b3dae68717cc28078
Héte Caetano <hetecaetano@gmail.com>
2021-01-22 17:12:52 -0300

    Adicionado o evento de click no botão Add to Cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,8 @@ const router = async () => {
 
   const main = document.getElementById('main-container');
   main.innerHTML = await screen.render();
+  await screen.after_render();
+
 };
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
