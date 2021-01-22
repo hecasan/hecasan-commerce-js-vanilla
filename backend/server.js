434454ea23c4fc51f779c39ea57a7f986131d7ad
Héte Caetano <hetecaetano@gmail.com>
2021-01-22 01:10:44 -0300

    Criado a página detalhes do produto

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,17 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Produto não encontrado' });
+  }
+
+
+});
+
 app.listen(5000, () => {
   console.log('Servidor em http://localhost:5000');
 });
