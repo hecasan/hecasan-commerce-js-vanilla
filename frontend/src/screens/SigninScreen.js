f0aadecd497e5ace1dea0e492f46a849cec94bfa
Héte Caetano <hetecaetano@gmail.com>
2021-01-24 16:48:04 -0300

    Add a rota da pagina de login e css

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -2,31 +2,32 @@ const SigninScreen = {
   after_render: () => { },
   render: () => {
     return `
-      <div class="form-container">
-        <form id="signin-form">
-          <ul class="form-itens">
-            <li>
-              <h1>Fazer login</h1>
-            </li>
-            <li>
-              <label for="email">E-mail</label>
-              <input type="email" name="email" id="email"/>
-            </li>
-            <li>
-              <label for="password">Senha</label>
-              <input type="password" name="password" id="password"/>
-            </li>
-            <li>
-              <button type="submit" class="primary">Entrar</button>
-            </li>
-            <li>
-              <div>
-                Novo aqui? <a href="/#/register">Crie uma conta</a>
-              </div>
-            </li>
-          </ul>
-        </form>
-      </div>
+    <div class="form-container">
+      <form id="signin-form">
+        <ul class="form-items">
+          <li>
+            <h1>Sign-In</h1>
+          </li>
+          <li>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" />
+          </li>
+          <li>
+            <label for="password">Senha</label>
+            <input type="password" name="password" id="password" />
+          </li>
+          <li>
+            <button type="submit" class="primary">Entrar</button>
+          </li>
+          <li>
+            <div>
+              Novo por aqui?
+              <a href="/#/register">Crie uma conta </a>
+            </div>
+          </li>
+        </ul>
+      </form>
+    </div>
     `;
   },
 };
